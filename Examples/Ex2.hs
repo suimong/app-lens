f864337d8b5add34e38edcd86ac07c72bca0caac
Kazutaka Matsuda <kztk@is.s.u-tokyo.ac.jp>
2015-02-27 22:50:19 +0900

    Non paper examples. I found a problem in treating lines.

---
+++ Examples/Ex2.hs
@@ -0,0 +1,167 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
+{- |
+This file contains some additional examples.
+-}
+
+module Examples.Ex2 where
+
+import Data.ApplicativeBX
+import Data.List (elemIndices, splitAt)
+
+import Data.Traversable (Traversable)
+import Data.Foldable    (Foldable) 
+
+import qualified Data.Map as M 
+import Data.Maybe
+import Data.List (nub, findIndex) 
+
+import Control.Applicative ((<$>))
+import Control.Category ((.))
+import Prelude hiding ((.))
+
+filterM p []     = return []
+filterM p (x:xs) =
+  do b <- liftO p x
+     if b then
+       (x :) <$> filterM p xs
+     else
+       filterM p xs
+
+filterL p = unliftMT (\xs -> sequenceL <$> filterM p xs)
+
+{-
+*Examples.Ex2> get (filterL (>2)) [1,2,3,4]
+[3,4]
+*Examples.Ex2> put (filterL (>2)) [1,2,3,4] [4,5]
+[1,2,4,5]
+*Examples.Ex2> put (filterL (>2)) [1,2,3,4] [0,3]
+*** Exception: Changing Observation
+*Examples.Ex2> put (filterL (>2)) [1,2,3,4] [1,2,3]
+*** Exception: Shape Mitmatch
+*Examples.Ex2> put (filterL (>2)) [1,2,3,4] [1]
+*** Exception: Shape Mitmatch
+-}
+
+reverseL = unliftT (\xs -> sequenceL $ reverse xs) 
+
+{-
+*Examples.Ex2> get reverseL [1,2,3]
+[3,2,1]
+*Examples.Ex2> put reverseL [1,2,3] [4,5,6]
+[6,5,4]
+*Examples.Ex2> put reverseL [1,2,3] [4,5]
+*** Exception: Shape Mitmatch
+*Examples.Ex2> put reverseL [1,2,3] [4,5,6,7]
+*** Exception: Shape Mitmatch
+-}
+
+
+{- Just to show the connection, we implement Voigtlander's bff -}
+bff :: (Traversable t1, Traversable t2, Eq a, Eq (t1 ()), Eq (t2 ())) =>
+       (forall s. t1 (L s a) -> t2 (L s b)) -> Lens (t1 a) (t2 b)
+bff f = unliftT (\t -> sequenceL $ f t)
+
+{- The following is the bff in our previous work in PPDP 13 -}
+bffM :: (Traversable t1, Traversable t2, Eq a, Eq (t1 ()), Eq (t2 ())) =>
+        (forall s. t1 (L s a) -> R s (t2 (L s b))) ->
+        Lens (t1 a) (t2 b)
+bffM f = unliftMT (\t -> sequenceL <$> f t)
+
+
+
+mynub :: Eq b => [L s b] -> R s (L s [b])
+mynub = fmap sequenceL . go
+    where
+      go []     = return []
+      go (x:xs) = do xs' <- delete x xs 
+                     r   <- go xs'
+                     return $ x:r
+
+      delete x []     = return []
+      delete x (y:ys) =
+          do b <- liftO2 (==) x y 
+             r <- delete x ys
+             if b then 
+                 return r 
+             else
+                 return $ y:r
+                  
+data EList a = EList [Int] (M.Map Int a) 
+             deriving (Eq, Show, Functor, Foldable, Traversable) 
+
+encode :: Eq a => [a] -> EList a
+encode xs = let ys = nub xs 
+            in EList [ fromJust $ findIndex (==x) ys | x <- xs]
+                     (M.fromList $ zip [0..] ys)
+
+decode :: EList a -> [a]
+decode (EList is m) =
+    [ fromJust $ M.lookup k m | k <- is ]
+
+mynubL1 :: Eq a => Lens [a] [a]
+mynubL1 = unliftMT mynub
+
+mynubL2 :: Eq a => Lens [a] [a]
+mynubL2 = unliftMT (\xs -> mynub (decode xs)) . encodeL
+
+encodeL = Lens encode (const decode) 
+
+{-
+*Examples.Ex2> get mynubL1 [1,1,2,3,2]
+[1,2,3]
+*Examples.Ex2> put mynubL1 [1,1,2,3,2] [1,2,6]
+[1,1,2,6,2]
+*Examples.Ex2> put mynubL1 [1,1,2,3,2] [5,6,7]
+*** Exception: Changing Observatio
+*Examples.Ex2>
+*Examples.Ex2> get mynubL2 [1,1,2,3,2]
+[1,2,3]
+*Examples.Ex2> put mynubL2 [1,1,2,3,2] [1,2,6]
+[1,1,2,6,2]
+*Examples.Ex2> put mynubL2 [1,1,2,3,2] [5,6,7]
+[5,5,6,7,6]
+-}
+
+
+
+
+
+
+{-
+MEMO: If we try to write bidirectional version of "lines" in our
+framework, we should encounter a limitation of our work.
+
+If we straightforwardly write a lifted monadic "lines", if
+from the codes in Prelude, it would have type 
+
+  linesM :: [L s Char] -> L s [[Char]]
+
+because the code contains character-wise construction of string due to
+"span".
+
+One would expect that its lifted function should have the type
+
+  linesM :: L s String -> [L s String]
+
+However, this has a problem we cannot change the length of the source
+string because "lub" only accepts the strings with the same length.
+To make "lub" effectively, we have to know that the input string is
+essentially a list of line-separated string. This means that we have
+to give a lens corresponding to "lines" manually.
+
+A lesson learned: to use our framework effectively, get must be a
+function from a data that have more-explicit structures to ones that
+have less-explicit structures.
+-}
+
+
+
+
+
+
+
+
+
